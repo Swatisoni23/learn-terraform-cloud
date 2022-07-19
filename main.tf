97c2022a4181acdb5d5e9a421436a817f5e3907e
“Swatisoni23” <soni23swati@gmail.com>
2022-07-20 00:00:29 +0100

    policy_commit_iam

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
-provider "aws" {
-    region = var.region
-    }
+resource "aws_iam_policy" "policy" {
+  name        = "${random_pet.pet_name.id}-policy"
+  description = "My test policy"
+  policy = "data.aws_iam_policy_document.example.json"
+}
+data "aws_iam_policy_document" "example" {
+  statement {
+    actions   = ["s3:ListAllMyBuckets"]
+    resources = ["arn:aws:s3:::*"]
+    effect = "Allow"
+  }
+  statement {
+    actions   = ["s3:*"]
+    resources = [aws_s3_bucket.bucket.arn]
+    effect = "Allow"
+  }
+}
+resource "aws_iam_user_policy_attachment" "attachment" {
+  user       = aws_iam_user.new_user.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+output "rendered_policy" {
+  value = data.aws_iam_policy_document.example.json
+}
