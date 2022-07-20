122a26823b901976b172507199128005ea064951
“Swatisoni23” <soni23swati@gmail.com>
2022-07-20 08:55:39 +0100

    addpolicy

--- variables.tf
+++ variables.tf
@@ -1,12 +1,26 @@
-variable "username" {
-  type    =     list
-  default = ["swati", "soni"]
-}
+module "iam_group_with_policies" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
 
+  name = "superadmins"
+
+  group_users = [
+    "swati",
+    "soni"
+  ]
+
+  attach_iam_self_management_policy = true
+
+  custom_group_policy_arns = [
+    "arn:aws:iam::aws:policy/AdministratorAccess",
+  ]
+
+  custom_group_policies = [
+    {
+      name   = "AllowS3Listing"
+      policy = data.aws_iam_policy_document.sample.json
+    }
+  ]
+}
 variable "region" {
   default = "eu-west-2"
-}
-
-variable "groupname" {
-  default = ["usergrouptest"]
 }
